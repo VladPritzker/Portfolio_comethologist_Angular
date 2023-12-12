13030c15b9f1991e97c6dc150472e1c9d5adacf4
vlad <pritzkervlad@gmail.com>
2023-12-12 01:33:53 -0500

    Your commit message here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'my-angular-app';
+
 }
